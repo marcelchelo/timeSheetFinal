b548ca1d3e956b37b12b526825d4391c1f32f69a
Marcelo <marcelo95326@icloud.com>
2022-05-16 23:05:40 -0400

    Added error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log("server...")
+const express =  require ('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const PORT =  process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+app.get("/", (req,res)=>{
+    res.status(200)
+    res.json({
+        message: 'Welcome to FingerIOT'
+    })
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes')) 
+app.use(errorHandler)
+
+
+app.listen(PORT, ()=>console.log(`Server Started on port ${PORT}`))
