eab40e6fd2e4d6bd3597bf19aa31395278324d4d
Marcelo <marcelo95326@icloud.com>
2022-05-19 14:38:59 -0400

    added protected route with MiddleWare

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,18 @@
 const express = require ("express")
 const router = express.Router()
-const {checkUser, registerUser, loginUser} = require('../controllers/userController')
+const {checkUser, registerUser,
+     loginUser,userDashboard} = require('../controllers/userController')
+
+const {protect} = require ('../middleware/authMiddleware')
+
+
 
 router.post('/', checkUser ,registerUser) 
 
 router.post('/login', loginUser)
+
+//protected route
+
+router.get('/dashboard', protect  ,userDashboard)
 
 module.exports = router
