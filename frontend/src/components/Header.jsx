c2790543a8aee17547a1246f7f48cf65184e35b3
Marcelo <marcelo95326@icloud.com>
2022-05-24 14:41:10 -0400

    UPDATE HEADER ADDER LOGOUT BUTTON

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,49 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
+import {useDispatch, useSelector} from 'react-redux'
+
+import {logout, reset} from '../features/auth/authSlice'
 
 function Header (){
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user} = useSelector((state)=> state.auth)
+    
+
+    const onLogout = () =>{
+        dispatch(logout())
+        dispatch(reset())
+        navigate("/")
+    }
+
     return(
-        <hader className="header">
+        <header className="header">
             <div className='logo'>
             <Link to='/'> MEGA Software Solutions</Link>
             </div>
            
             <div>
                 <ul>
+                    {user? (
+                        <li>
+                            <button className='btn' onClick={onLogout}> <FaSignOutAlt/> LogOut</button>
+                        </li>
+                    ) : (
+                    <>
                     <li>
                        <Link to="/login"> <FaSignInAlt/> LogIn</Link> 
                     </li>
                     <li>
                        <Link to="/register"><FaUser/> Register</Link> 
                     </li>
+                    </>
+                    ) }
+                    
                 </ul>
             </div>
-        </hader>
+        </header>
     )
 }
 
