076681b57afec03a4956a6b4b587c1d2d302f2d8
Marcelo <marcelo95326@icloud.com>
2022-05-24 14:41:28 -0400

    added logOut Service

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -18,7 +18,10 @@ const register = async (userData) => {
 
 const logout = () => localStorage.removeItem('user')
 
-const authService = {register,
-                    logout}
+const authService = {
+    register,
+    logout,
+   
+  }
 
 export default authService
