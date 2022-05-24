45d2504989a27054b9f33a3639724fb44b645511
Marcelo <marcelo95326@icloud.com>
2022-05-24 13:02:51 -0400

    exported Reset
    message const returns undefined not sure why

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-
+import authService from './authService'
 const initialState = {
     user: 'null',
     isError: false,
@@ -14,7 +14,14 @@ const initialState = {
 export const register = createAsyncThunk('auth/register', 
     async(user, thunkAPI) =>{
         //thunkAPI to connect to backEnd 
-         console.log(user)
+        try {
+            return await authService.register(user)
+        } catch (error) {
+            const message = (error.response && error.respnse.data
+                && error.respnse.data.message) || error.message || error.toString()
+
+                return thunkAPI.rejectWithValue(message)
+        }
 })
 
 
@@ -28,9 +35,35 @@ export const login = createAsyncThunk('auth/login',
 export const authSlice  = createSlice ({
     name: 'auth',
     initialState,
-    reducers:{},
+    reducers:{
+
+        //no asyncThunk required so, reset added here.
+        reset: (state =>{
+            state.isLoading = false
+            state.isSuccess = false
+            state.isError =false
+            state.message = ''
+        })
+    },
     extraReducers: (builder) =>{
-        
+        builder
+            .addCase(register.pending, (state) =>{
+                state.isLoading =true
+         })
+            .addCase(register.fulfilled, (state, action)=>{
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(register.rejected, (state, action)=>{
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
+
     }
 })
+
+export const {reset} = authSlice.actions
 export default authSlice.reducer
