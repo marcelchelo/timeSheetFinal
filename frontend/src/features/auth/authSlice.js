4e82ba1174ae11692709ae69dae4d66610a84ece
Marcelo <marcelo95326@icloud.com>
2022-05-24 14:42:00 -0400

    added redux state case for logout fullfilled

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -39,6 +39,11 @@ export const login = createAsyncThunk('auth/login',
          console.log(user)
 })
 
+//logout User
+export const logout = createAsyncThunk('auth/logout', async () => {
+    await authService.logout()
+  })
+
 export const authSlice  = createSlice ({
     name: 'auth',
     initialState,
@@ -66,6 +71,9 @@ export const authSlice  = createSlice ({
                 state.isLoading = false
                 state.isError = true
                 state.message = action.payload
+                state.user = null
+            })
+            .addCase(logout.fulfilled, (state) => {
                 state.user = null
             })
 
