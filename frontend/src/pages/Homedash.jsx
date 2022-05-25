903523f68a51712f66da334add889a3d8fa4a00f
Marcelo <marcelo95326@icloud.com>
2022-05-25 13:28:45 -0400

    dashboard after login

---
+++ frontend/src/pages/Homedash.jsx
@@ -0,0 +1,24 @@
+import {Link} from 'react-router-dom'
+import {FaDigitalTachograph , FaHardHat} from 'react-icons/fa'
+function Homedash (){
+    return(
+        <>
+        <section className='heading'>
+        <p>Please choose from an option below</p>
+        </section>
+        
+
+
+        <section >
+            <div>
+
+            <Link to="/dashboard/users" className='btn btn-block btn-reverse'> <FaHardHat/> Search by Workers</Link> 
+
+            <Link to="/dashboard/" className='btn btn-block btn-reverse'  > <FaDigitalTachograph/> Search by Sites</Link> 
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default Homedash
