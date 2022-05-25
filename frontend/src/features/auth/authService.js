ccd550b4f2351c0562062d340c86157ee9dda95d
Marcelo <marcelo95326@icloud.com>
2022-05-25 13:29:45 -0400

    complete auth Service

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
 
-const API_URL = '/api/users'
+const API_URL_REGISTER = '/api/users'
+const API_URL_LOGIN = '/api/users/login'
+
 
 //register user 
 
 const register = async (userData) => {
-    const response = await axios.post(API_URL, userData)
+    const response = await axios.post(API_URL_REGISTER, userData)
 
     if(response.data){
         localStorage.setItem('user', JSON.stringify(response.data))
@@ -15,12 +17,24 @@ const register = async (userData) => {
 }
 
 //logOut
-
 const logout = () => localStorage.removeItem('user')
+
+
+//login
+
+const login = async(userData) =>{
+    const response = await axios.post(API_URL_LOGIN,userData)
+
+    if(response.data){
+        localStorage.setItem('user',JSON.stringify(response.data))
+    }
+    return response.data
+}
 
 const authService = {
     register,
     logout,
+    login,
    
   }
 
