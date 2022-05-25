1d211b60279993bf20061ceab385a0da6db1809f
Marcelo <marcelo95326@icloud.com>
2022-05-25 13:29:25 -0400

    new Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,29 @@
-import {Link} from 'react-router-dom'
-import {FaDigitalTachograph , FaHardHat} from 'react-icons/fa'
-function Home (){
-    return(
+
+const Home= ()=>{
+    return (
         <>
-        <section className='heading'>
-        <p>Please choose from an option below</p>
-        </section>
-        
 
-
-        <section >
-            <div>
-
-            <Link to="/dashboard/users" className='btn btn-block btn-reverse'> <FaHardHat/> Search by Workers</Link> 
-
-            <Link to="/dashboard/" className='btn btn-block btn-reverse'  > <FaDigitalTachograph/> Search by Sites</Link> 
-            </div>
-        </section>
+            <section>
+                <div>
+                <h1 style={{color: "red"}}>The problem:</h1>
+                </div>
+                    <p> I'm a subcontractor, working at different sites. Therefore I'm not at every jobsite. 
+                        so I trust my workers arrive and leave on time. We do have a sign in sheet after all.     
+                    </p>
+                    <p>
+                        Typically in NYC we start at 7am and leave at 3:30pm . And we all know workers arrive on time, and trust they leave on time. 
+                        
+                        The reality is they do neither Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, voluptatem. Provident enim, ex optio ea itaque nulla qui nemo, rem ipsa alias animi voluptatum ipsum nisi. Distinctio itaque atque facere!
+                    </p>
+                <div>
+                    <h1>Peace of mind</h1>
+                    <p> Say goodbye to looking up where your workers were </p>
+                    <h1>Secure</h1>
+                    <p>Convinient</p>
+                    <h3></h3>
+                </div>
+            </section>
+           
         </>
     )
 }
